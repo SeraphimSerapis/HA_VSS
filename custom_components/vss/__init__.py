5457bcf66a7f30cbe1ff0c3c8a04bd5c853b9f32
Tim Messerschmidt <timmesserschmidt@gmail.com>
2024-07-04 11:08:31 +0200

    Update __init__.py
    Implements await for loading and unloading components.

--- custom_components/vss/__init__.py
+++ custom_components/vss/__init__.py
@@ -21,25 +21,11 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up VSS from a config entry."""
     hass.data[DOMAIN][entry.entry_id] = Device(hass, entry.data["host"])
 
-    for component in PLATFORMS:
-        hass.async_create_task(
-            hass.config_entries.async_forward_entry_setup(entry, component)
-        )
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     return True
 
 
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
-    unload_ok = all(
-        await asyncio.gather(
-            *[
-                hass.config_entries.async_forward_entry_unload(entry, component)
-                for component in PLATFORMS
-            ]
-        )
-    )
-    if unload_ok:
-        hass.data[DOMAIN].pop(entry.entry_id)
-
-    return unload_ok
+    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
